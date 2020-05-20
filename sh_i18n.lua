3a9adce96443a2bc832e6e4667ef33c15b07ec19
GlorifiedPig <killingpigs123@gmail.com>
2020-05-20 12:23:58 +0200

    thanks devul, fuck you sleepy

--- sh_i18n.lua
+++ sh_i18n.lua
@@ -10,7 +10,7 @@
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ]]--
 
-local i18nVersion = 1.1
+local i18nVersion = 1.2
 
 if not i18n or i18n.Version < i18nVersion then
     i18n = {
@@ -36,6 +36,6 @@ if not i18n or i18n.Version < i18nVersion then
         local finalPhrase = registeredPhrases["en"][phraseIdentifier]
         if phraseLanguage[phraseIdentifier] then finalPhrase = phraseLanguage[phraseIdentifier] end
 
-        return table.Count( { ... } ) > 0 and string.format( finalPhrase, ... ) or finalPhrase
+        return #{ ... } > 0 and string.format( finalPhrase, ... ) or finalPhrase
     end
 end
